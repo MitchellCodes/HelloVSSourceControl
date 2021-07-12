9b5b00eb3328408b538e5af00f203418a9342443
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-07-11 17:31:17 -0700

    Changed welcome message

--- HelloVSSourceControl/Program.cs
+++ HelloVSSourceControl/Program.cs
@@ -6,7 +6,7 @@ namespace HelloVSSourceControl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the dealership manager!");
         }
     }
 }
