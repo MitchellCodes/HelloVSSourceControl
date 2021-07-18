41dcc457adc059d79ee4570d2e5d482eed8fac4e
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-07-17 20:09:42 -0700

    Added vehicle class

---
+++ HelloVSSourceControl/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloVSSourceControl
+{
+    /// <summary>
+    /// Represents a single vehicle
+    /// with the number in stock
+    /// </summary>
+    class Vehicle
+    {
+        /// <summary>
+        /// The make of the vehicle
+        /// ex. Honda
+        /// </summary>
+        public string Make { get; set; }
+
+        /// <summary>
+        /// The model of the vehicle
+        /// ex. Prelude
+        /// </summary>
+        public string Model { get; set; }
+
+        /// <summary>
+        /// The quantity of the vehicles in stock
+        /// </summary>
+        public byte QuantityInStock { get; set; }
+    }
+}
